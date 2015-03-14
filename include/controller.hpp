22844e8717211716262d1fca55ccd284aeac8315
norkos <norbert.kosmal@gmail.com>
2015-03-14 12:32:17 +0100

    Crash fixt

--- include/controller.hpp
+++ include/controller.hpp
@@ -36,8 +36,8 @@ public:
     typedef mpl::vector<idle> initial_state;
     
     BOOST_MSM_EUML_DECLARE_TRANSITION_TABLE((
-        board_scrolling() == idle() [anonymous()] / (init_board()),
-        game_over() == board_scrolling() + window_close()
+       // board_scrolling() == idle() [anonymous()] / (init_board()),
+        game_over() == idle() + window_close()
         
     ), transition_table)
     
